4745980ebbeb196dda8107460b4f61312e5ef679
Sanchos-dev <sanchos@sanchos.su>
2026-09-04 23:18:58 +0300

    update

---
+++ server/config.py
@@ -0,0 +1,6 @@
+#SanChat server configuration 
+
+server_api_port = 19840
+server_domain = "chat.example.com" #you can use ur domain here
+server_id = "" #leave empty if u want it random 
+#sever_id needs to be unique

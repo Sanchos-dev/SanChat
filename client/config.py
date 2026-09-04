f0f1e86fd041bbdb8bc91f2091d7799930cc8d2c
Sanchos-dev <sanchos@sanchos.su>
2026-09-05 00:25:03 +0300

    update

--- client/config.py
+++ client/config.py
@@ -1,4 +1,6 @@
 DEBUG = True
 server_api_port = 19840
-server_domain = ''
-server_id = ''
+server_domain = ""
+server_id = ""
+client_id = ""
+first_time = True
--- client/main.py
+++ client/main.py
@@ -3,14 +3,16 @@ import time
 
 import ui
 import client
-import config as conf
+import config
+
+ui.init()
 
 
-def save_conf(inst, val):
+### I will start with ui
+def save_config(inst, val):
     setattr(conf, inst, val)
     with open("config.py", "w", encoding="utf-8") as f:
         for k, v in vars(conf).items():
             if not k.startswith("__"):
                 f.write(f"{k} = {repr(v)}\n")
 
-save_conf("DEBUG",True)
